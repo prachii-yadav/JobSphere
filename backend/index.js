7174daa1939d56cbdbc9fff2a788e252665d57f2
prachii yadav <prachiyadav7324@gmail.com>
2025-06-02 00:17:00 +0530

    Initial Commit

---
+++ backend/index.js
@@ -0,0 +1,29 @@
+// const express = require('express')  //old way
+import express from "express";
+import cookieParser from "cookie-parser";
+import cors from "cors";
+import dotenv from "dotenv";
+const app = express();
+
+// basic get api
+// app.get("/home",(req,res)=>{
+//     return res.status(200).json({
+//         message:"I am coming from backend",
+//         success: true
+//     })
+// })
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+app.use(cookieParser());
+const corsOptions = {
+    origin:'http//localhost:5173',
+    credentials:true
+}
+app.use(cors(corsOptions));
+
+const PORT = 3000;
+app.listen(PORT,()=>{
+    console.log(`Server running at port ${PORT}`);
+})

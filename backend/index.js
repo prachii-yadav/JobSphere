35fe288556b8b8303c86e464baa3902e036f3262
Prachi Yadav <prachiahasolar@gmail.com>
2025-06-04 19:32:30 +0530

    working on company module

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,8 @@ import cookieParser from "cookie-parser";
 import cors from "cors";
 import dotenv from "dotenv";
 import connectDB from "./utils/db.js";
+import userRoute from "./routes/user.route.js";
+import companyRoute from "./routes/company.route.js";
 dotenv.config({}); 
 const app = express();
 
@@ -26,6 +28,11 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 const PORT = process.env.PORT || 3000;
+
+// api's
+app.use("/api/v1/user", userRoute);
+app.use("/api/v1/company", companyRoute);
+
 app.listen(PORT,()=>{
     connectDB();
     console.log(`Server running at port ${PORT}`);

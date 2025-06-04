df0dd4214576e2035dab66f94faf7f5ce2375c43
prachii yadav <prachiyadav7324@gmail.com>
2025-06-05 00:26:05 +0530

    patch fix

--- backend/routes/job.route.js
+++ backend/routes/job.route.js
@@ -1,6 +1,6 @@
 import express from "express";
 import isAuthenticated from "../middlewares/isAuthenticated.js";
-import { postJob } from "../controllers/job.controller.js";
+import { getAdminJobs, getAllJobs, getJobById, postJob } from "../controllers/job.controller.js";
 
 const router = express.Router();
 router.route("/post").post(isAuthenticated,postJob);

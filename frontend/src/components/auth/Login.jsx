bfe54918954ebc112b2b5ced64f2e83d8eb86014
prachii yadav <prachiyadav7324@gmail.com>
2025-06-14 23:15:08 +0530

    profile page

--- frontend/src/components/auth/Login.jsx
+++ frontend/src/components/auth/Login.jsx
@@ -10,7 +10,7 @@ import axios from "axios";
 import { USER_API_END_POINT } from "../../utils/constant";
 import { useDispatch, useSelector } from "react-redux";
 import { Loader2 } from "lucide-react";
-import { setLoading } from "../../redux/authSlice";
+import { setLoading, setUser } from "../../redux/authSlice";
 
 const Login = () => {
   const [input, setInput] = useState({
@@ -37,6 +37,7 @@ const Login = () => {
         withCredentials: true,
       });
       if (res.data.success) {
+        dispatch(setUser(res.data.user));
         navigate("/");
         toast.success(res.data.message);
       }
